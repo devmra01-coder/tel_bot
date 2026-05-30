ee169d6cdc1df1bbc1d5cdbf9676ce9584825936
Mr.A <Dev.mr.A2001@gmail.com>
2026-05-30 16:41:21 +0100

    v.0.1.2

--- bot.php
+++ bot.php
@@ -2,18 +2,18 @@
 
 file_put_contents('log.txt', file_get_contents('php://input'), FILE_APPEND);
 
-$telegram_ip_ranges = [
-    ['lower' => '149.154.160.0', 'upper' => '149.154.175.255'], // literally 149.154.160.0/20
-    ['lower' => '91.108.4.0',    'upper' => '91.108.7.255'],    // literally 91.108.4.0/22
-];
-$ip_dec = (float) sprintf("%u", ip2long($_SERVER['REMOTE_ADDR']));
-$ok = false;
-foreach ($telegram_ip_ranges as $telegram_ip_range) if (!$ok) {
-    $lower_dec = (float) sprintf("%u", ip2long($telegram_ip_range['lower']));
-    $upper_dec = (float) sprintf("%u", ip2long($telegram_ip_range['upper']));
-    if ($ip_dec >= $lower_dec and $ip_dec <= $upper_dec) $ok = true;
-}
-if (!$ok) die("Sik!");
+// $telegram_ip_ranges = [
+//     ['lower' => '149.154.160.0', 'upper' => '149.154.175.255'], // literally 149.154.160.0/20
+//     ['lower' => '91.108.4.0',    'upper' => '91.108.7.255'],    // literally 91.108.4.0/22
+// ];
+// $ip_dec = (float) sprintf("%u", ip2long($_SERVER['REMOTE_ADDR']));
+// $ok = false;
+// foreach ($telegram_ip_ranges as $telegram_ip_range) if (!$ok) {
+//     $lower_dec = (float) sprintf("%u", ip2long($telegram_ip_range['lower']));
+//     $upper_dec = (float) sprintf("%u", ip2long($telegram_ip_range['upper']));
+//     if ($ip_dec >= $lower_dec and $ip_dec <= $upper_dec) $ok = true;
+// }
+// if (!$ok) die("Sik!");
 //--------------------------------------------------------------------------------------------
 
 
