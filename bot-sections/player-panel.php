26bebb82f13859e92e35179271eccaa78fe2f888
Mr.A <devmr.a01@gmail.com>
2026-06-12 20:42:59 +0100

    jh

--- bot-sections/player-panel.php
+++ bot-sections/player-panel.php
@@ -1,6 +1,6 @@
 <?php
 $botText = "⚜️- به ربات بازی خوش آمدید.";
-$teamText = $playerCheck == "Yes" ? "" : "➖➖➖➖➖➖➖➖\n🤖 ساخته شده توسط هکتوریم\n📣 Channel : @Hector_Bots\n👤 سفارش ربات : @HectorTMSupport";
+$teamText = $playerCheck == "Yes" ? "" : "➖➖➖➖➖➖➖➖\n🤖 ساخته شده توسط دوست ممدقلی استاد هکر برنامه نویس اقای مهندس Mr.A\n📣 Channel : چنل دارم ولی لینکشو نمیدم چون دوست ندارم\n👤 سفارش ربات : @Dev_MrA  ";
 
 if ($text == "پنل" && $playerStep == "none") {
     if (!$playerCheck) {
@@ -16,7 +16,7 @@ if ($text == "پنل" && $playerStep == "none") {
     $conn->query("UPDATE `$citiesTable` SET `step`='none' WHERE `city id`='{$chat_id}'LIMIT 1");
 }
 
-if ($data == "back") {
+if ($text == "back") {
     bot('EditMessageText', [
         'chat_id' => $chat_id,
         'message_id' => $message_id,
