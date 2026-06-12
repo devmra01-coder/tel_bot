d4a37c40486da74d92bf57244721b7643831e362
Mr.A <devmr.a01@gmail.com>
2026-06-12 19:43:50 +0100

    drop

--- bot.php
+++ bot.php
@@ -47,7 +47,29 @@ $citySoldiersTable = "$pastName-citySoldiers";
 $cityPeopleTable = "$pastName-cityPeople";
 $cityCampsTable = "$pastName-cityCamps";
 
- // $conn->query("INSERT INTO `{$buildingsTable}` (`english name`) VALUES ('gfdgerg')");
+$tables = [
+    $itemsTable, 
+    $soldiersTable, 
+    $peopleTable, 
+    $buildingsTable, 
+    $campsTable, 
+    $citiesTable, 
+    $adminsTable, 
+    $cityBuildingsTable, 
+    $cityItemsTable, 
+    $citySoldiersTable, 
+    $cityPeopleTable, 
+    $cityCampsTable
+];
+ 
+foreach ($tables as $table) {
+    mysqli_query($conn, "DROP TABLE IF EXISTS `$table`");
+    echo "جدول $table حذف شد.<br>";
+}
+ 
+
+echo "<h3>تمام جداول با موفقیت پاک شدند.</h3>"; 
+
 //----------important files-------------
 include './telegram.php';
 include './functions.php';
