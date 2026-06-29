6a78ac517aa80fa0cd08d4f2aeaca751cf918934
Mr.A <devmr.a01@gmail.com>
2026-06-29 14:13:18 +0100

    kjuhgf

--- functions.php
+++ functions.php
@@ -626,3 +626,63 @@ function logUpgrade($conn, $city_id, $item_name) {
     mysqli_query($conn, "INSERT INTO `upgrade_daily_log` (`city_id`,`item_name`,`date`) VALUES ('{$city_id}','{$item_name}','{$today}') 
                          ON DUPLICATE KEY UPDATE `date`=`date`");
 }
+
+
+
+
+
+
+
+
+
+
+
+
+function getAllGameItems($conn) {
+    $items = [];
+    $tables = ['itemsTable', 'peopleTable', 'soldiersTable'];
+    
+    foreach ($tables as $tableVar) {
+        global $$tableVar;
+        $table = $$tableVar;
+        $q = mysqli_query($conn, "SELECT `english name` as english, `persian name` as persian FROM `$table`");
+        while ($row = mysqli_fetch_assoc($q)) {
+            $items[] = $row;
+        }
+    }
+    return $items;
+}
+
+function showCostSelectionKeyboard($conn, $chat_id, $targetItem) {
+    $allItems = getAllGameItems($conn);
+    $keyboard = ['inline_keyboard' => []];
+
+    foreach ($allItems as $item) {
+        $keyboard['inline_keyboard'][] = [[
+            'text' => "💰 " . $item['persian'],
+            'callback_data' => "cost_" . $targetItem . "_" . $item['english']
+        ]];
+    }
+    $keyboard['inline_keyboard'][] = [['text' => "✅ تمام شد", 'callback_data' => "cost_done_" . $targetItem]];
+
+    bot('sendMessage', [
+        'chat_id' => $chat_id,
+        'text' => "انتخاب کنید کدام آیتم هزینه خرید باشد:",
+        'reply_markup' => json_encode($keyboard)
+    ]);
+}
+function showUpgradeCostSelection($conn, $chat_id, $targetItem, $level) {
+    $allItems = getAllGameItems($conn);
+    $keyboard = ['inline_keyboard' => []];
+    foreach ($allItems as $item) {
+        $keyboard['inline_keyboard'][] = [[
+            'text' => "💰 " . $item['persian'] . " (سطح $level)",
+            'callback_data' => "upgcost_{$level}_" . $item['english']
+        ]];
+    }
+    bot('sendMessage', [
+        'chat_id' => $chat_id,
+        'text' => "هزینه‌های سطح $level را انتخاب کنید:",
+        'reply_markup' => json_encode($keyboard)
+    ]);
+}
