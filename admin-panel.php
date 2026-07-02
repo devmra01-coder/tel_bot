3085e241bf6ab41e0e58498667d45ab3a7462e8a
Mr.A <devmr.a01@gmail.com>
2026-07-02 22:54:27 +0100

    hgnghn

--- admin-panel.php
+++ admin-panel.php
@@ -23,12 +23,12 @@ if (in_array($from_id, $admins)) {
     }
     if ($text == "🛒 مدیریت ایتم های خرید" && $theAdminStep == "none") {
         $theText = "📙 به بخش مدیریت آیتم ها خرید خوش آمدید. لطفا از منوی زیر عملیات مورد نظر خود را انتخاب کنید.";
-        SendMessage($chat_id, $theText, "HTML", $message_id, $UpgradePanel);
+        SendMessage($chat_id, $theText, "HTML", $message_id, $ShopPanel);
         $conn->query("UPDATE `$adminsTable` SET `step`='none' WHERE `id`='{$from_id}'LIMIT 1");
     }
     if ($text == "🛒 مدیریت ایتم های ارتقا" && $theAdminStep == "none") {
         $theText = "📙 به بخش مدیریت آیتم ها ارتقا خوش آمدید. لطفا از منوی زیر عملیات مورد نظر خود را انتخاب کنید.";
-        SendMessage($chat_id, $theText, "HTML", $message_id, $itemsPanel);
+        SendMessage($chat_id, $theText, "HTML", $message_id, $UpgradePanel);
         $conn->query("UPDATE `$adminsTable` SET `step`='none' WHERE `id`='{$from_id}'LIMIT 1");
     }
     if ($text == "👤 مدیریت شخصیت ها" && $theAdminStep == "none") {
